9a514ef020584d7542049360d193bdb307941f83
randomserj <pakhomov.sergei.v@gmail.com>
2021-04-13 00:52:16 +0200

    code cleaned up a bit *oled to be moved to periphery.py

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
+import gc
 import network
 import webrepl
-from machine import UART
-from machine import Pin
+from machine import UART, Pin, I2C
 import _thread as thread
 import canbus_trx as trx
+import sh1106
+import misc
 
+MODULE_NAME = 'MAIN'
 
+PIN_I2C_SCK = Pin(22)
+PIN_I2C_SDA = Pin(21)
 LED_WIFI = Pin(2, Pin.OUT)
-LED_CANBUS_RX = Pin(19, Pin.OUT)
-LED_CANBUS_TX = Pin(21, Pin.OUT)
-
-THREAD_CANBUS_RX_FLAG_EXIT = True
-THREAD_CANBUS_TX_FLAG_EXIT = True
-
-
-def init_state():
-    global THREAD_CANBUS_RX_FLAG_EXIT
-    THREAD_CANBUS_RX_FLAG_EXIT = True
-    global THREAD_CANBUS_TX_FLAG_EXIT
-    THREAD_CANBUS_TX_FLAG_EXIT = True
-    LED_WIFI.off()
-    LED_CANBUS_RX.off()
-    LED_CANBUS_TX.off()
-    tja.can.clear_rx_queue()
-    tja.can.clear_tx_queue()
 
 
 def wlan_connect(ssid='Somewifi', password='iwannawifi'):
@@ -38,37 +26,8 @@ def wlan_connect(ssid='Somewifi', password='iwannawifi'):
     print('network config:', wlan.ifconfig())
 
 
-def thread_canbus_rx():
-    while tja.can.any() and not THREAD_CANBUS_RX_FLAG_EXIT:
-        frame = tja.rx_frame()
-        uart.write(bytearray(tja.frame_to_string(frame) + '\n\r'))
-    tja.can.clear_rx_queue()
-    LED_CANBUS_RX.off()
-    thread.exit()
-
-
-def dev_ctrl(cmd, param=None):
-    if cmd == 'reset':
-        init_state()
-    if cmd == 'wifi':
-        wlan_connect()
-    if cmd == 'rx_stop':
-        global THREAD_CANBUS_RX_FLAG_EXIT
-        THREAD_CANBUS_RX_FLAG_EXIT = True
-    if cmd == 'rx':
-        global THREAD_CANBUS_RX_FLAG_EXIT
-        THREAD_CANBUS_RX_FLAG_EXIT = False
-        LED_CANBUS_RX.on()
-        thread.start_new_thread(thread_canbus_rx, ())
-    if cmd == 'tx':
-        print(param)
-    if cmd == 'exit':
-        uart.sendbreak()
-    if cmd == 'test':
-        uart.write(bytearray('it is still alive\n\r'))
-    print(cmd)
-
-
+gc.collect()
 webrepl.start()
+
+oled = sh1106.SH1106_I2C(128, 64, I2C(-1, scl=PIN_I2C_SCK, sda=PIN_I2C_SDA))
 tja = trx.CanBusDevice()
-uart = UART(1, 115200, tx=17, rx=16)
